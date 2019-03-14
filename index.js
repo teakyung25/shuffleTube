fc1f30cb7c53179c4d2cd8591bda15c71105cea3
teakyung25 <31352970+teakyung25@users.noreply.github.com>
2019-03-13 21:59:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,75 @@
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
+  var GoogleAuth;
+  var SCOPE = 'https://www.googleapis.com/auth/youtube.force-ssl';
+  function handleClientLoad() {
+    // Load the API's client and auth2 modules.
+    // Call the initClient function after the modules load.
+    gapi.load('client:auth2', initClient);
+  }
 
-var GoogleAuth; // Google Auth object.
-var isAuthorized;
-var currentApiRequest;
+  function initClient() {
+    // Retrieve the discovery document for version 3 of YouTube Data API.
+    // In practice, your app can retrieve one or more discovery documents.
+    var discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest';
 
-function initClient() {
-  gapi.client.init({
-      'apiKey': 'AIzaSyBHSirs_C3123cabefj2z9Fzlacpf59RMQ',
-      'clientId': '923096734869-isnlkutlsp2kgc5j2210qnhacjmlgrqj.apps.googleusercontent.com',
-      'scope': 'https://www.googleapis.com/auth/youtube.force-ssl',
-      'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
-  }).then(function () {
+    // Initialize the gapi.client object, which app uses to make API requests.
+    // Get API key and client ID from API Console.
+    // 'scope' field specifies space-delimited list of access scopes.
+    gapi.client.init({
+        'apiKey': 'AIzaSyBHSirs_C3123cabefj2z9Fzlacpf59RMQ',
+        'discoveryDocs': [discoveryUrl],
+        'clientId': '923096734869-isnlkutlsp2kgc5j2210qnhacjmlgrqj.apps.googleusercontent.com',
+        'scope': SCOPE
+    }).then(function () {
       GoogleAuth = gapi.auth2.getAuthInstance();
 
       // Listen for sign-in state changes.
       GoogleAuth.isSignedIn.listen(updateSigninStatus);
-  });
-}
 
-/**
- * Store the request details. Then check to determine whether the user
- * has authorized the application.
- *   - If the user has granted access, make the API request.
- *   - If the user has not granted access, initiate the sign-in flow.
- */
-function sendAuthorizedApiRequest(requestDetails) {
-  currentApiRequest = requestDetails;
-  if (isAuthorized) {
-    // Make API request
-    // gapi.client.request(requestDetails)
+      // Handle initial sign-in state. (Determine if user is already signed in.)
+      var user = GoogleAuth.currentUser.get();
+      setSigninStatus();
 
-    // Reset currentApiRequest variable.
-    currentApiRequest = {};
-  } else {
-    GoogleAuth.signIn();
+      // Call handleAuthClick function when user clicks on
+      //      "Sign In/Authorize" button.
+      $('#sign-in-or-out-button').click(function() {
+        handleAuthClick();
+      }); 
+      $('#revoke-access-button').click(function() {
+        revokeAccess();
+      }); 
+    });
   }
-}
 
-/**
- * Listener called when user completes auth flow. If the currentApiRequest
- * variable is set, then the user was prompted to authorize the application
- * before the request executed. In that case, proceed with that API request.
- */
-function updateSigninStatus(isSignedIn) {
-  if (isSignedIn) {
-    isAuthorized = true;
-    if (currentApiRequest) {
-      sendAuthorizedApiRequest(currentApiRequest);
+  function handleAuthClick() {
+    if (GoogleAuth.isSignedIn.get()) {
+      // User is authorized and has clicked 'Sign out' button.
+      GoogleAuth.signOut();
+    } else {
+      // User is not signed in. Start Google auth flow.
+      GoogleAuth.signIn();
     }
-  } else {
-    isAuthorized = false;
   }
-}
+
+  function revokeAccess() {
+    GoogleAuth.disconnect();
+  }
+
+  function setSigninStatus(isSignedIn) {
+    var user = GoogleAuth.currentUser.get();
+    var isAuthorized = user.hasGrantedScopes(SCOPE);
+    if (isAuthorized) {
+      $('#sign-in-or-out-button').html('Sign out');
+      $('#revoke-access-button').css('display', 'inline-block');
+      $('#auth-status').html('You are currently signed in and have granted ' +
+          'access to this app.');
+    } else {
+      $('#sign-in-or-out-button').html('Sign In/Authorize');
+      $('#revoke-access-button').css('display', 'none');
+      $('#auth-status').html('You have not authorized this app or you are ' +
+          'signed out.');
+    }
+  }
+
+  function updateSigninStatus(isSignedIn) {
+    setSigninStatus();
+  }
