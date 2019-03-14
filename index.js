982597b2caab5567dcca7aa8d7200ed372f974ef
teakyung25 <31352970+teakyung25@users.noreply.github.com>
2019-03-13 21:47:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ function onSignIn(googleUser) {
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
+var GoogleAuth; // Google Auth object.
+function initClient() {
+  gapi.client.init({
+      'apiKey': 'AIzaSyBHSirs_C3123cabefj2z9Fzlacpf59RMQ',
+      'clientId': '923096734869-isnlkutlsp2kgc5j2210qnhacjmlgrqj.apps.googleusercontent.com',
+      'scope': 'https://www.googleapis.com/auth/youtube.force-ssl',
+      'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
+  }).then(function () {
+      GoogleAuth = gapi.auth2.getAuthInstance();
+
+      // Listen for sign-in state changes.
+      GoogleAuth.isSignedIn.listen(updateSigninStatus);
+  });
+}
